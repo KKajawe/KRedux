2e8473e6c86d59a9a156f0f655ac2c5b981106ae
Kalyani Kajawe <kajawek@gmail.com>
2021-02-25 15:21:59 +0100

    Issue: Redux-Store #7 Third core concept store implemeted.

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-console.log("from Index.js")
+const redux = require('redux')
+const createStore = redux.createStore
+
+
 const BUY_CAKE = 'BUY_CAKE'
+
 
 //Action creator
 function buyCake() {
@@ -14,7 +18,7 @@ const initialState = {
     numOfCakes: 10
 }
 
-//reducer fun
+//reducer fun - require to make state transition based on action received by store
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case BUY_CAKE: return {
@@ -24,3 +28,20 @@ const reducer = (state = initialState, action) => {
         default: return state
     }
 }
+
+//holds app state - createstore accepts reducer fun as parameter which contains app state 
+const store = createStore(reducer)
+
+//Store provides method getState() to get current state 
+console.log('Initial state', store.getState())
+
+//Allow app to subscribe to changes in state in store
+const unsubscribe = store.subscribe(() => console.log('updated state', store.getState()))
+
+//store provides dispatch method to update state
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+
+//unsubscribe by calling fun return by subscribe method
+unsubscribe()
