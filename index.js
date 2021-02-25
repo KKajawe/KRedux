b82262afbc8573864d710b699dda68d0039cdd83
Kalyani Kajawe <kajawek@gmail.com>
2021-02-25 14:47:01 +0100

    Issue: Redux-Reducers #5 Implemented Reducer function which is second core concept of Redux

--- index.js
+++ index.js
@@ -2,10 +2,25 @@ console.log("from Index.js")
 const BUY_CAKE = 'BUY_CAKE'
 
 //Action creator
-function buyCake(){
+function buyCake() {
     //return action object
     return {
         type: BUY_CAKE,
         info: 'First Redux action'
+    }
+}
+//state of app is object
+const initialState = {
+    numOfCakes: 10
+}
+
+//reducer fun
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case BUY_CAKE: return {
+            ...state,  //copy state object
+            numOfCakes: state.numOfCakes - 1 // only changes property that need to be
+        }
+        default: return state
     }
 }
